6551795e503273eab6927f9f53d5752987c5a633
ROBOT <tkata12@outlook.jp>
2025-11-16 02:48:14 +0000

    fix: eslint gitignore path

--- scripts/template/eslint.config.js
+++ scripts/template/eslint.config.js
@@ -1,12 +1,11 @@
 import prettier from "eslint-config-prettier";
 import js from "@eslint/js";
-import globals from "globals";
 import tseslint from "typescript-eslint";
 import { includeIgnoreFile } from "@eslint/compat";
 import { defineConfig } from "eslint/config";
 import { fileURLToPath } from "node:url";
 
-const gitignorePath = fileURLToPath(new URL("../.gitignore", import.meta.url));
+const gitignorePath = fileURLToPath(new URL("../../.gitignore", import.meta.url));
 
 export const config = defineConfig([
 	includeIgnoreFile(gitignorePath),
