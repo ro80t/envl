e3e70135c2c2fc87bb352a3ab74aae64796fe8e2
ROBOT <tkata12@outlook.jp>
2025-12-05 11:48:22 +0000

    add: envl-js-test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+tests_dir=$(cd $(dirname $0); cd ../tests; pwd)
+tests=(
+    "envl-js-test"
+)
+
 cargo test --verbose
+
+for test_dir in ${tests[@]}; do
+    cd $tests_dir
+    cd $test_dir
+    ./test.sh
+done
