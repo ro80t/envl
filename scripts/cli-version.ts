3f57b4882a71e4df1e6a948ed8840ef96ddf3a2f
ROBOT <tkata12@outlook.jp>
2025-12-22 01:33:14 +0000

    fix: unify the envl-cli version

--- scripts/cli-version.ts
+++ scripts/cli-version.ts
@@ -6,11 +6,11 @@ import pkgJson from "../packages/envl-cli/package.json";
 
 const __dirname = import.meta.dirname;
 
-const cargoToml = parse(
-    await readFile(join(__dirname, "../packages/envl-cli/Cargo.toml"), "utf-8")
-) as {
-    package?: {
-        version?: string;
+const cargoToml = parse(await readFile(join(__dirname, "../Cargo.toml"), "utf-8")) as {
+    workspace?: {
+        package?: {
+            version?: string;
+        };
     };
 };
 
@@ -18,8 +18,8 @@ if (!pkgJson.version) {
     throw new Error("Version is not found in package.json");
 }
 
-if (cargoToml.package && cargoToml.package.version) {
-    if (pkgJson.version === cargoToml.package.version) {
+if (cargoToml.workspace && cargoToml.workspace.package && cargoToml.workspace.package.version) {
+    if (pkgJson.version === cargoToml.workspace.package.version) {
         console.log("Versions in Cargo.toml and package.json are identical");
     } else {
         throw new Error("Versions in Cargo.toml and package.json aren't identical");
